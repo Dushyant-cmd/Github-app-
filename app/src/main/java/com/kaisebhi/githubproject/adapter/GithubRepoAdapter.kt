a903d4eabbc3865bfc4ed3cad84c704a5dca6683
Dushyant Sharma <Dushyant.977360@gmail.com>
2023-07-05 11:30:45 +0530

    Action completed project not published before

--- app/src/main/java/com/kaisebhi/githubproject/adapter/GithubRepoAdapter.kt
+++ app/src/main/java/com/kaisebhi/githubproject/adapter/GithubRepoAdapter.kt
@@ -1,14 +1,18 @@
 package com.kaisebhi.githubproject.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Typeface
+import android.net.Uri
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.kaisebhi.githubproject.R
@@ -28,11 +32,25 @@ class GithubRepoAdapter(private val context: Context, private val viewModel: Mai
         val itemDataModel = getItem(position)
         holder.itemNameTv.text = "Name: " + itemDataModel.name
         holder.itemDescTv.text = "Description: " + itemDataModel.description
+        holder.root.setOnClickListener(View.OnClickListener {
+            val intent: Intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse(itemDataModel.html_url))
+            context.startActivity(intent)
+        })
+
+        holder.shareRL.setOnClickListener(View.OnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.putExtra(Intent.EXTRA_TEXT, "Repository name: ${itemDataModel.name} Click here to view Repository: ${itemDataModel.html_url}")
+            intent.type = "text/plain"
+            context.startActivity(intent)
+        })
     }
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val itemNameTv = view.findViewById<TextView>(R.id.itemName)
         val itemDescTv = view.findViewById<TextView>(R.id.desc)
+        val root = view.findViewById<CardView>(R.id.root)
+        val shareRL = view.findViewById<RelativeLayout>(R.id.shareRL)
     }
 
     class DiffUtils : DiffUtil.ItemCallback<AllRepoEntity>() {
--- app/src/main/java/com/kaisebhi/githubproject/data/network/ApiInterface.kt
+++ app/src/main/java/com/kaisebhi/githubproject/data/network/ApiInterface.kt
@@ -10,6 +10,6 @@ interface ApiInterface {
     suspend fun getAllRepo(@Path("owner") ownerName: String): Response<List<AllRepoEntity>>
 
     @POST("orgs/{owner}/repos")
-    @Headers("Authorization: Bearer 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")
+    @Headers("Authorization: Bearer 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")
     suspend fun addRepo(@Path("owner") ownerName: String, @Body addData: AddData): Response<AllRepoEntity>
 }
